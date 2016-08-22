46348fce479f59ddf9b5584db6b79c7741d25b48
steveskimmel <steveskimmel@gmail.com>
2016-08-22 22:39:22 +0200

    'test'

--- app/controllers/spree/ww_edits/user_registrations_controller_decorator.rb
+++ app/controllers/spree/ww_edits/user_registrations_controller_decorator.rb
@@ -1,10 +1,6 @@
 Spree::UserRegistrationsController.class_eval do
   def create
     @user = build_resource(spree_user_params)
-    cookies[:user_first_name] = @user.first_name
-    cookies[:user_last_name] = @user.last_name
-    cookies[:user_ww_card_number] = @user.ww_card_number
-    cookies[:user_email] = @user.email
     resource_saved = resource.save
     yield resource if block_given?
     if resource_saved
@@ -15,6 +11,10 @@ Spree::UserRegistrationsController.class_eval do
         sign_up(resource_name, resource)
         session[:spree_user_signup] = true
         respond_with resource, location: after_sign_up_path_for(resource)
+        cookies[:user_first_name] = @user.first_name
+        cookies[:user_last_name] = @user.last_name
+        cookies[:user_ww_card_number] = @user.ww_card_number
+        cookies[:user_email] = @user.email
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
