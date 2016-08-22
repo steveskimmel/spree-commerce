80bf94fb56c9a52152b8d2833d7e5f945aecda7c
steveskimmel <steveskimmel@gmail.com>
2016-08-22 14:19:00 +0200

    test

--- app/overrides/ww_edits/edit_confirm_order_ui.rb
+++ app/overrides/ww_edits/edit_confirm_order_ui.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_order_details',
   :name => 'edit_confirm_order_ui',
-  :replace => '[data-hook="order_details_line_items_headers"]',
+  :replace => 'table#line-items',
   :partial => "ww_edits/edit_confirm_order_ui"
   )
