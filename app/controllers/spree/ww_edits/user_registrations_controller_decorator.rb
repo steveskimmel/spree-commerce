6c7a7300cf536d392fecc2fbada20c4dc22aed40
steveskimmel <steveskimmel@gmail.com>
2016-08-22 22:24:45 +0200

    test

--- app/controllers/spree/ww_edits/user_registrations_controller_decorator.rb
+++ app/controllers/spree/ww_edits/user_registrations_controller_decorator.rb
@@ -1,7 +1,7 @@
 Spree::UserRegistrationsController.class_eval do
   def create
     @user = build_resource(spree_user_params)
-    cookies[:user_first_name] = @user.email
+    cookies[:user_first_name] = @user.first_name
     cookies[:user_last_name] = @user.last_name
     cookies[:user_ww_card_number] = @user.ww_card_number
     cookies[:user_email] = @user.email
